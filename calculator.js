5f1d64e05f6f6703ff46ee1bf8147596666a974e
Dylan Taft <dylbronjames@gmail.com>
2019-09-02 16:23:19 -0600

    added bmi section. needs button and finishing touches

--- calculator.js
+++ calculator.js
@@ -16,6 +16,20 @@ var num2 = Number(req.body.n2);
 var result = num1 + num2;
 
   res.send("The Result of the calculation is " + result);
+});
+
+app.get("/bmicalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+
+  var bmi = weight/ (height * height);
+
+  res.send("Your BMi is " + bmi);
 })
 
 app.listen(3000, function() {

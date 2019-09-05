82df098a4719421252e9795aaff30a78887ad9da
Dylan Taft <dylbronjames@gmail.com>
2019-09-04 21:51:45 -0600

    added multiple functions, needs if else, and links

--- calculator.js
+++ calculator.js
@@ -2,48 +2,51 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/root.html");
 })
 
-app.get("/math", function(req, res) {
+app.get("/math", function (req, res) {
   res.sendFile(__dirname + "/math.html");
 });
 
-app.post("/math", function(req, res) {
+app.post("/math", function (req, res) {
 
-var num1 = Number(req.body.n1);
-var num2 = Number(req.body.n2);
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
 
-var add = num1 + num2;
-var subtract = num1 - num2;
-var multiply = num1 * num2;
-var divide = num1 / num2;
+  var add = num1 + num2;
+  var subtract = num1 - num2;
+  var multiply = num1 * num2;
+  var divide = num1 / num2;
 
-//create if statements for each button click to add multiply etc...
-if(req="add") {
-  res.send("Answer: " + add);
-}
+  res.write("For Addition, answer is: " + add + "\n");
+  res.write("For Subtraction, answer is: " + subtract + "\n");
+  res.write("For Multiplication, answer is: " + multiply + "\n");
+  res.write("For Division, answer is: " + divide + "\n");
+  res.write("\n");
+  // res.write("<li><a href='/'>Home</a></li")
 
+  res.send();
 
 });
 
-app.get("/bmi", function(req, res) {
+app.get("/bmi", function (req, res) {
   res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/bmi", function(req, res) {
+app.post("/bmi", function (req, res) {
 
   var height = parseFloat(req.body.height);
   var weight = parseFloat(req.body.weight);
-//fix bmi calculation. possibly in metric or off a decimal?
-  var bmi = weight/ (height * height);
+  //fix bmi calculation. possibly in metric or off a decimal?
+  var bmi = weight / (height * height);
 
   res.send("Your BMI is " + bmi);
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("server running on port 3000");
 })
